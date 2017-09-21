6dce12c9b313ba381ebc5bef7be08ccf9cb8e568
Eky Fauzi <ekyfauzi@hotmail.com>
2017-09-21 20:52:23 +0700

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
   root to: "home#index"
 
   devise_for :users, controllers: {
-    sessions: 'users/sessions'
+    sessions: 'users/sessions',
+    registrations: 'users/registrations',
+    passwords: 'users/passwords'
   }
 
   resources :settings, only: [:new, :create]
